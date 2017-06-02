6f0c7a176a3732fa9839b8a306d6d324917bde80
xfl <xfl@ncmem.com>
2017-06-02 17:09:02 +0800

    修复skygqbox在xp部分浏览器中无法关闭弹出窗口的问题

--- WordPaster/js/WordPaster.js
+++ WordPaster/js/WordPaster.js
@@ -315,7 +315,7 @@ function WordPasterManager()
 	};
 	this.CloseDialogFile = function ()
 	{
-	    $('#wrapClose').click();
+		$.skygqbox.hide();
 	};
 
     //打开粘贴图片对话框
@@ -325,7 +325,7 @@ function WordPasterManager()
 	};
 	this.CloseDialogPaste = function ()
 	{
-	    $('#wrapClose').click();
+		$.skygqbox.hide();
 	};
 	this.InsertHtml = function (html)
 	{
@@ -337,8 +337,6 @@ function WordPasterManager()
 	this.SetEditor = function (edt)
 	{
 	    _this.Editor = edt;
-	    //_this.WordPaster.Editor = edt;
-	    //_this.ImagePaster.Editor = edt;
 	};
 
     //单击按钮粘贴
@@ -350,7 +348,6 @@ function WordPasterManager()
 	    }
 	    if (!this.chrome45 && !_this.edge)
 	    {
-	        
             this.app.paste();
 	    }
 	    else if (this.chrome45)
@@ -361,7 +358,7 @@ function WordPasterManager()
 	    {
             this.app.paste();
 	    }
-	};	    
+	};
 
     //单击按钮粘贴
 	this.PasteManual = function ()
@@ -592,7 +589,7 @@ function WordPasterManager()
 	};
 	this.load_complete_edge = function (json)
 	{
-        _this.app.init();
+		_this.app.init();
     };
     this.load_complete = function (json)
     {
@@ -605,7 +602,7 @@ function WordPasterManager()
             }
         }
         if (needUpdate) this.need_update();
-        else { $('#wrapClose').click(); }
+        else { $.skygqbox.hide(); }
     };
     this.recvMessage = function (msg)
 	{
